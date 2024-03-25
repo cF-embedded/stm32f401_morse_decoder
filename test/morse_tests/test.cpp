618b6f22ad60ed9ccf411dfa3384acffa20fc45b
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-25 19:50:28 +0100

    projects: add morse tests

---
+++ test/morse_tests/test.cpp
@@ -0,0 +1,12 @@
+/******************************************************************************
+ *brief: Morse decoder TDD file
+ *author: cF-embedded.pl
+ ******************************************************************************/
+
+extern "C"
+{
+#include "../../src/morse/morse.h"
+#include "mocks/button_hardware_mock.h"
+}
+
+#include <gtest/gtest.h>
