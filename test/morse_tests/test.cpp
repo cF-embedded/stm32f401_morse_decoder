8feb5e1667d47facd65687acb238efbe5998e24e
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-26 19:36:07 +0100

    test: add 4 test

--- test/morse_tests/test.cpp
+++ test/morse_tests/test.cpp
@@ -37,15 +37,6 @@ TEST_F(morse_decoder_test, morse_init_state_after_init)
     ASSERT_EQ(morse_decoder.morse_state, MORSE_INIT);
 }
 
-TEST_F(morse_decoder_test, morse_button_is_prssed_state_after_pressed_button)
-{
-    set_mock_button_hardware_read_state(BUTTON_STATE_PRESSED);
-
-    morse_decoder_start(&morse_decoder);
-
-    ASSERT_EQ(morse_decoder.morse_state, MORSE_BUTTON_IS_PRESSED);
-}
-
 TEST_F(morse_decoder_test, morse_led_is_on_after_pressed_button)
 {
     set_mock_button_hardware_read_state(BUTTON_STATE_PRESSED);
@@ -53,4 +44,22 @@ TEST_F(morse_decoder_test, morse_led_is_on_after_pressed_button)
     morse_decoder_start(&morse_decoder);
 
     ASSERT_TRUE(morse_decoder.led);
+}
+
+TEST_F(morse_decoder_test, morse_buzzer_is_on_after_pressed_button)
+{
+    set_mock_button_hardware_read_state(BUTTON_STATE_PRESSED);
+
+    morse_decoder_start(&morse_decoder);
+
+    ASSERT_TRUE(morse_decoder.buzzer);
+}
+
+TEST_F(morse_decoder_test, morse_button_is_prssed_state_after_pressed_button)
+{
+    set_mock_button_hardware_read_state(BUTTON_STATE_PRESSED);
+
+    morse_decoder_start(&morse_decoder);
+
+    ASSERT_EQ(morse_decoder.morse_state, MORSE_BUTTON_IS_PRESSED);
 }
