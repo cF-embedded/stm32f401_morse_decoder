cee5e9e39c9cbe674841575ded625446ee37a5b8
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-26 19:14:12 +0100

    morse: add morse_states_e_t

--- src/morse/morse.h
+++ src/morse/morse.h
@@ -7,20 +7,24 @@
 #define __MORSE_H
 
 #include "../../button/api/button.h"
+#include "../../timer/api/timer_hardware.h"
 #include "morse_type.h"
 #include "stdbool.h"
 #include "stdint.h"
 
 typedef struct
 {
-    uint32_t timer;
-    morse_char_t morse_char;
+    timer_hardware_s_t timer;
+    morse_char_s_t morse_char;
     bool led;
     bool buzzer;
     button_s_t* button;
     uint32_t oled_display;
+    morse_states_e_t morse_state;
 } morse_decoder_s_t;
 
-void morse_decoder_init(morse_decoder_s_t*);
+void morse_decoder_init(morse_decoder_s_t*, timer_hardware_s_t, button_s_t*);
+void morse_decoder_start(morse_decoder_s_t*);
+morse_char_s_t get_morse_decoder_char(morse_decoder_s_t*);
 
 #endif /* __MORSE_H */
--- src/morse/morse_type.h
+++ src/morse/morse_type.h
@@ -15,6 +15,14 @@ typedef struct
 {
     dot_t dot;
     dash_t dash;
-} morse_char_t;
+} morse_char_s_t;
+
+typedef enum
+{
+    MORSE_INIT,
+    MORSE_BUTTON_IS_PRESSED,
+    MORSE_BUTTON_STATE_RELEASED,
+    MORSE_BREAK_BETTWEN_CHAR_ELEMENTS_ELAPSED,
+} morse_states_e_t;
 
 #endif /* __MORSE_TYPES_H */
