9fac106162f77e50e16a279ff728e8bce372e818
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-26 19:31:40 +0100

    test: add 2 test

--- test/morse_tests/test.cpp
+++ test/morse_tests/test.cpp
@@ -36,3 +36,12 @@ TEST_F(morse_decoder_test, morse_init_state_after_init)
 {
     ASSERT_EQ(morse_decoder.morse_state, MORSE_INIT);
 }
+
+TEST_F(morse_decoder_test, morse_button_is_prssed_state_after_pressed_button)
+{
+    set_mock_button_hardware_read_state(BUTTON_STATE_PRESSED);
+
+    morse_decoder_start(&morse_decoder);
+
+    ASSERT_EQ(morse_decoder.morse_state, MORSE_BUTTON_IS_PRESSED);
+}
