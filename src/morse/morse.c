3417263827dc04137194a91b3ac7402848706854
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-26 18:47:05 +0100

    morse: add morse_decoder init procedure

--- src/morse/morse.c
+++ src/morse/morse.c
@@ -5,13 +5,23 @@
  ******************************************************************************/
 
 #include "morse.h"
+#include "morse_times.h"
 #include <string.h>
 
-void morse_decoder_init(morse_decoder_s_t* morse_decoder)
+void morse_decoder_init(morse_decoder_s_t* morse_decoder, timer_hardware_s_t _timer, button_s_t* _button)
 {
+    morse_decoder->button = _button;
+    morse_decoder->timer = _timer;
     morse_decoder->button->hardware.button_hardware_init();
-    morse_decoder->led = 0;
+    morse_decoder->timer.timer_hardware_init();
+    morse_decoder->led = false;
     morse_decoder->buzzer = false;
-    morse_decoder->timer = 0;
-    memset(&morse_decoder->morse_char, 0, sizeof(morse_char_t));
+    memset(&morse_decoder->morse_char, 0, sizeof(morse_char_s_t));
+}
+
+void morse_decoder_start(morse_decoder_s_t* morse_decoder) {}
+
+morse_char_s_t get_morse_decoder_char(morse_decoder_s_t* morse_decoder)
+{
+    morse_char_s_t morse_char_tmp = {0, 0};
 }
