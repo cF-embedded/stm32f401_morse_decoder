56002f7ee59efb02c53c8b95867ddb0beacdcb97
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-26 18:48:04 +0100

    test: add SetUp and TearDown methods

--- test/morse_tests/test.cpp
+++ test/morse_tests/test.cpp
@@ -6,7 +6,20 @@
 extern "C"
 {
 #include "../../src/morse/morse.h"
-#include "mocks/button_hardware_mock.h"
+#include "mocks/button/button_hardware_mock.h"
+#include "mocks/timer/timer_hardware_mock.h"
 }
 
 #include <gtest/gtest.h>
+
+class morse_decoder_test : public ::testing::Test
+{
+  protected:
+    void SetUp() override
+    {
+        mock_button_hardware_init();
+        mock_timer_hardware_init();
+    }
+
+    void TearDown() override {}
+};
