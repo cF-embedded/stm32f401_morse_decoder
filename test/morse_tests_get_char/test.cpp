2b4319b5c6a2ef06a7aaf3d6a9d218b7bc6a4a52
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-28 19:16:13 +0100

    morse_tests_get_char: add tests others ascii chars

--- test/morse_tests_get_char/test.cpp
+++ test/morse_tests_get_char/test.cpp
@@ -49,3 +49,195 @@ TEST_F(morse_decoder_get_char_test, morse_get_A_char)
     char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
     ASSERT_EQ(morse_char, 'A');
 }
+
+TEST_F(morse_decoder_get_char_test, morse_get_B_char)
+{
+    strcpy(morse_decoder.morse_char, "-...");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'B');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_C_char)
+{
+    strcpy(morse_decoder.morse_char, "-.-.");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'C');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_D_char)
+{
+    strcpy(morse_decoder.morse_char, "-.. ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'D');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_F_char)
+{
+    strcpy(morse_decoder.morse_char, "..-.");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'F');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_G_char)
+{
+    strcpy(morse_decoder.morse_char, "--. ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'G');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_H_char)
+{
+    strcpy(morse_decoder.morse_char, "....");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'H');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_I_char)
+{
+    strcpy(morse_decoder.morse_char, "..  ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'I');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_J_char)
+{
+    strcpy(morse_decoder.morse_char, ".---");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'J');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_K_char)
+{
+    strcpy(morse_decoder.morse_char, "-.- ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'K');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_L_char)
+{
+    strcpy(morse_decoder.morse_char, ".-..");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'L');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_M_char)
+{
+    strcpy(morse_decoder.morse_char, "--  ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'M');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_N_char)
+{
+    strcpy(morse_decoder.morse_char, "-.  ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'N');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_O_char)
+{
+    strcpy(morse_decoder.morse_char, "--- ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'O');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_P_char)
+{
+    strcpy(morse_decoder.morse_char, ".--.");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'P');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_Q_char)
+{
+    strcpy(morse_decoder.morse_char, "--.-");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'Q');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_R_char)
+{
+    strcpy(morse_decoder.morse_char, ".-. ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'R');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_S_char)
+{
+    strcpy(morse_decoder.morse_char, "... ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'S');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_T_char)
+{
+    strcpy(morse_decoder.morse_char, "-   ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'T');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_U_char)
+{
+    strcpy(morse_decoder.morse_char, "..- ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'U');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_V_char)
+{
+    strcpy(morse_decoder.morse_char, "...-");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'V');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_W_char)
+{
+    strcpy(morse_decoder.morse_char, ".-- ");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'W');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_X_char)
+{
+    strcpy(morse_decoder.morse_char, "-..-");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'X');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_Y_char)
+{
+    strcpy(morse_decoder.morse_char, "-.--");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'Y');
+}
+
+TEST_F(morse_decoder_get_char_test, morse_get_Z_char)
+{
+    strcpy(morse_decoder.morse_char, "--..");
+
+    char morse_char = morse_decoder_get_decoded_char(&morse_decoder);
+    ASSERT_EQ(morse_char, 'Z');
+}
