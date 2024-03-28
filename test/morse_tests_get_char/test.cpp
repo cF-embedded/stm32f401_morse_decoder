10d6ed3efbb468420f3a260f643315ad08295c54
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-28 18:03:23 +0100

    morse_tests_get_char: add mocks

--- test/morse_tests_get_char/test.cpp
+++ test/morse_tests_get_char/test.cpp
@@ -6,12 +6,15 @@
 extern "C"
 {
 #include "../../src/morse/morse.h"
-#include "../../src/morse/morse_times.h"
+#include "../../src/morse/morse_type.h"
+#include "../mocks/button/button_hardware_mock.h"
+#include "../mocks/timer/timer_hardware_mock.h"
+#include <string.h>
 }
 
 #include <gtest/gtest.h>
 
-class morse_decoder_test : public ::testing::Test
+class morse_decoder_get_char_test : public ::testing::Test
 {
   protected:
     morse_decoder_s_t morse_decoder;
@@ -19,9 +22,11 @@ class morse_decoder_test : public ::testing::Test
     {
         button_s_t button;
         button_hardware_s_t hardware_button;
+        setup_button_hardware_with_mocks(&hardware_button);
         button_init(&button, hardware_button);
 
         timer_hardware_s_t hardware_timer;
+        setup_timer_hardware_with_mocks(&hardware_timer);
 
         morse_decoder_init(&morse_decoder, hardware_timer, &button);
     }
