9164cac32bd04a5d370a12fb4a6a90db6098dc0f
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-30 10:16:50 +0100

    project: add display_hardware

---
+++ display/api/display_hardware.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ *brief: Display driver hardware interface header file
+ *author: cF-embedded.pl
+ ******************************************************************************/
+
+#ifndef __DISPLAY_HARDWARE_H
+#define __DISPLAY_HARDWARE_H
+
+typedef struct
+{
+    void (*display_hardware_init)(void);
+    void (*display_hardware_set_char)(char);
+    void (*display_hardware_clear)(void);
+    void (*display_hardware_set_header)(void);
+} display_hardware_s_t;
+
+#endif /* __DISPLAY_HARDWARE_H */
