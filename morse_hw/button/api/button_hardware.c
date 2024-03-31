40a497a15b5ea0336bfff66463b931fd5ed94adb
cF-embedded <133535477+cF-embedded@users.noreply.github.com>
2024-03-31 08:52:11 +0200

    button_hardware: change button logic

--- morse_hw/button/api/button_hardware.c
+++ morse_hw/button/api/button_hardware.c
@@ -11,7 +11,7 @@ void button_hardware_init(void)
     /*Configure GPIO pins : PE4 */
     GPIO_InitStruct.Pin = MORSE_BUTTON_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(MORSE_BUTTON_PORT, &GPIO_InitStruct);
 }
 
@@ -19,11 +19,11 @@ button_state_e_t button_hardware_read_state(void)
 {
     if(HAL_GPIO_ReadPin(MORSE_BUTTON_PORT, MORSE_BUTTON_PIN) == GPIO_PIN_SET)
     {
-        return BUTTON_STATE_PRESSED;
+        return BUTTON_STATE_RELEASED;
     }
     else
     {
-        return BUTTON_STATE_RELEASED;
+        return BUTTON_STATE_PRESSED;
     }
 }
 
